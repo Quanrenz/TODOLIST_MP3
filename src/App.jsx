2a9044660e3e1a966bd65009eab92f6a3628983d
Quanrenz <quanpham2082002@gmail.com>
2025-05-30 21:45:44 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,24 @@ import viteLogo from '/vite.svg'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [word, setword] = useState('')
+  console.log(word)
   return (
     <>
-      <h1>app</h1>
+      <div className="flex gap-8 h-screen items-center border border-red-500 justify-center">
+          <input 
+              type="text" 
+              className="outline-none border border-blue-600 px-4 py-2 w-[400px]"
+              value={word}
+              onChange={e => setword(e.target.value)}
+          />
+          <button
+           type="button"
+           className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white"
+          >
+              Add
+          </button>
+      </div>
     </>
   )
 }
