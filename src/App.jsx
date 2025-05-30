0b7f8c3a9f7b6b68293d9ca701915310c7462f3a
Quanrenz <quanpham2082002@gmail.com>
2025-05-30 23:02:12 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,16 @@ import viteLogo from '/vite.svg'
 
 function App() {
   const [word, setword] = useState('')
-  console.log(word)
+  const [todos, settodos] = useState([])
+  const handleAdd = ()=>{
+    settodos(prev => [...prev, word])
+    setword('')
+  }
   return (
     <>
-      <div className="flex gap-8 h-screen items-center border border-red-500 justify-center">
-          <input 
+      <div className="flex flex-col gap-8  h-screen items-center border border-red-500 justify-center">
+         <div className="flex gap-8">
+             <input 
               type="text" 
               className="outline-none border border-blue-600 px-4 py-2 w-[400px]"
               value={word}
@@ -18,12 +23,26 @@ function App() {
           <button
            type="button"
            className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white"
+           onClick={handleAdd}
           >
               Add
           </button>
+         </div>
+
+         <div className="font-bold text-xl">
+              <h3>Content: </h3>
+         </div>
+         <ul>
+          {todos?.map((item, index)  =>{
+            return(
+              <li key ={index}>{item}</li>
+            )
+          })}
+         </ul>
       </div>
     </>
   )
 }
+
 
 export default App
