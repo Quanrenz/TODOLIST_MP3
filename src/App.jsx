f57ece6ec532d86f6058da6bba5f87a6b4f41425
Quanrenz <quanpham2082002@gmail.com>
2025-05-31 00:24:14 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
+  import { ToastContainer, toast} from 'react-toastify'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
 
 function App() {
-  const [word, setword] = useState('')
+  const [work, setwork] = useState('')
   const [todos, settodos] = useState([])
   const handleAdd = ()=>{
-    settodos(prev => [...prev, word])
-    setword('')
+    if(todos?.some(item => item.id == work?.replace(/\s/g,''))){
+      toast.warn('Công việc đã được thêm vào trước đó')
+    }else{
+      settodos(prev => [...prev, {id: work?.replace(/\s/g,''), job: work}])
+      setwork('')
+    }
+  };
+  console.log(todos)
+  const handleDeleteJob = (id)=>{
+    settodos(prev => prev.filter(item => item.id != id))
   }
   return (
     <>
@@ -17,8 +26,8 @@ function App() {
              <input 
               type="text" 
               className="outline-none border border-blue-600 px-4 py-2 w-[400px]"
-              value={word}
-              onChange={e => setword(e.target.value)}
+              value={work}
+              onChange={e => setwork(e.target.value)}
           />
           <button
            type="button"
@@ -33,13 +42,29 @@ function App() {
               <h3>Content: </h3>
          </div>
          <ul>
-          {todos?.map((item, index)  =>{
+          {todos?.map((item)  =>{
             return(
-              <li key ={index}>{item}</li>
+              <li key ={item.id} className="flex gap-4 items-center ">
+                <span className='my-2'>{item.job}</span>
+                <span onClick={()=>handleDeleteJob(item.id)} className='my-2 hover:cursor-pointer p-2'>x</span>
+              </li>
             )
           })}
          </ul>
       </div>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </>
   )
 }
